91e806f60b55be6195f15863545e82425e5b7829
scottwegley <scottwegley@gmail.com>
2022-12-01 00:40:40 -0800

    Documentation

--- src/Util/Scheduler.java
+++ src/Util/Scheduler.java
@@ -7,12 +7,17 @@ import VexnetDriver.VEXnetDriver;
  * A Thread-Like class to store a FIFO queue of commands.
  * On run, these commands are iterated through and executed.
  * Previous commands must finish before continuing to the next.
+ * @author ScottWegley
+ * @version 1.0
  */
 public class Scheduler extends Thread {
     /**A FIFO LinkedList of {@link Command}'s to be executed sequentially.
      * Added to through {@link #add(Command)} and cleared with {@link #reset()}
      */
     private LinkedList<Command> commandQueue = new LinkedList<Command>();
+    /**A logging utility to output packets received 
+     * over the VEXnet while the scheduler remains active.
+     */
     private ReturnLogger logger;
 
     private VEXnetDriver driver;
@@ -49,6 +54,9 @@ public class Scheduler extends Thread {
         commandQueue.clear();
     }
 
+    /**Force the exit of the thread loop and the loops
+     * of all sub-threads.
+     */
     @Override
     public void interrupt() {
         logger.deactivate();
